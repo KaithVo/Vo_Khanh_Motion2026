f86609177388b809d3c795682e5db84a15cea61f
Khanh VO <vokar@sheridancollege.ca>
2026-09-11 12:08:12 -0400

    fix

--- Programming3-Motion/Assets/Scripts/SquareSpawner.cs
+++ Programming3-Motion/Assets/Scripts/SquareSpawner.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class test_script : MonoBehaviour
+public class SquareSpawner : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
